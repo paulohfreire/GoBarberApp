20acdb0e4da26436a3dc1d8e17901d790e93e26c
Paulo Henrique Freire <paulo.freire53@gmail.com>
2020-11-23 23:22:59 -0300

    Initial commit

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Title } from './styles';
+
+const Landing: React.FC = () => {
+    return (
+        <>
+            <Title>Hello World</Title>
+        </>
+    );
+};
+export default Landing;
