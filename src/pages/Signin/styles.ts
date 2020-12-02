99c3937e7d112458eb7750a319988a5732a72088
Paulohfreire <paulo.freire53@gmail.com>
2020-12-02 09:04:07 -0300

    fix:fixing signin styles

--- src/pages/Signin/styles.ts
+++ src/pages/Signin/styles.ts
@@ -54,32 +54,38 @@ export const AnimationContainer = styled.div`
         &:hover {
             color: ${shade(0.2, '#ff9000')};
         }
+    }
+    a {
+        color: #f4ede8; // > a siginifica que vai alterar somente o "a" mais externo
+        display: block;
 
-        a {
-            color: #f4ede8;
-            display: block; // o que faz?
+        margin: 20px 20px 15px 80px;
 
-            margin-top: 15px;
-            text-decoration: none;
-            transition: 0.2s;
-            &:hover {
-                color: ${shade(0.2, '#f4ede8')};
-            }
+        text-decoration: none;
+        transition: color 0.2s;
+
+        display: flex;
+        align-items: center;
+        &:hover {
+            color: ${shade(0.2, '#ff9000')};
         }
+    }
 
-        > a {
-            color: #ff9000; // > a siginifica que vai alterar somente o "a" mais externo
-            display: block;
-            margin: 15px 20px 15px 0;
+    > a {
+        color: #ff9000; // > a siginifica que vai alterar somente o "a" mais externo
+        display: block;
+        margin: 15px 20px 15px 0;
 
-            text-decoration: none;
-            transition: color 0.2s;
+        text-decoration: none;
+        transition: color 0.2s;
 
-            display: flex;
-            align-items: center;
-            svg {
-                margin-right: 10px;
-            }
+        display: flex;
+        align-items: center;
+        svg {
+            margin-right: 10px;
+        }
+        &:hover {
+            color: ${shade(0.2, '#ff9000')};
         }
     }
 `;
